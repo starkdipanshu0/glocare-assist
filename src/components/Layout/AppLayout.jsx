1f68c5e0f57ae5120244f9600ab89ac916927811
starkdipanshu0 <starkdipanshu456@gmail.com>
2024-10-24 21:11:14 +0530

    footar fix -1

--- src/components/Layout/AppLayout.jsx
+++ src/components/Layout/AppLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './Header'
-import Footer from './footer'
+import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
 
 function AppLayout() {
